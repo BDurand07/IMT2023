d4653c82130da819d5101350c926148dd0f0a461
j21lecou <116662687+j21lecou@users.noreply.github.com>
2023-03-14 17:26:45 +0100

    Update constantblackscholesprocess.hpp
    edited

--- constantblackscholesprocess.hpp
+++ constantblackscholesprocess.hpp
@@ -1,13 +1,30 @@
-
+#ifndef CONSTANTBLACKSCHOLESPROCESS_H
+#define CONSTANTBLACKSCHOLESPROCESS_H
 #include <ql/stochasticprocess.hpp>
 
 namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
 
-        // your implementation goes here
+    public:
+        ConstantBlackScholesProcess(double underlyingValue, double riskFreeRate, double volatility_, double dividend_);
+
+        // we modify methods that are used in the usual case and called during compilation
+        Real x0() const;
+        Real drift(Time t, Real x) const;
+        Real diffusion(Time t, Real x) const;
+        Real apply(Real x0, Real dx) const;
+
+
+    private:
+        double underlying_value;
+        double risk_free_rate;
+        double volatility;
+        double dividend;
         
     };
 
 }
+
+#endif
 
